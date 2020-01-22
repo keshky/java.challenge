0cf56f7866b351100e830f932091639a51b47b7b
keshky <mahmoud_elkeshky_90@hotmail.com>
2020-01-22 22:29:32 +0300

    first problem solving commit

---
+++ src/main/java/keshky/hackerrank/problem/solving/challenge/CompareTheTriplets.java
@@ -0,0 +1,10 @@
+package keshky.hackerrank.problem.solving.challenge;
+
+public class CompareTheTriplets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
